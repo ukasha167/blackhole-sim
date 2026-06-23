0af2e956022391e0274a06c0e638fe8f52836483
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-06-23 18:36:38 +0500

    REFACTOR: Port main to SDL3 and add renderer and solver

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,47 @@
-#include <raylib.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <algorithm>
 
 #include "defines.hpp"
+#include "renderer.hpp"
+#include "solver.hpp"
 
-int main()
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "HER EYES.");
-    SetTargetFPS(62);
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        DrawText(TextFormat("FPS: %d\n", GetFPS()), 10, 10, 24, WHITE);
-
-        EndDrawing();
+int main(int /*argc*/, char* /*argv*/[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return 1;
     }
 
-    CloseWindow();
+    bhs::Renderer renderer;
+    if (!renderer.init(bhs::kWindowTitle, bhs::kWindowWidth, bhs::kWindowHeight)) {
+        SDL_Log("Renderer init failed");
+        SDL_Quit();
+        return 1;
+    }
+
+    bhs::Solver solver;
+    solver.init();
+
+    bool running = true;
+    uint64_t lastTicks = SDL_GetTicksNS();
+
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) running = false;
+            if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) running = false;
+        }
+
+        const uint64_t nowTicks = SDL_GetTicksNS();
+        double dt = static_cast<double>(nowTicks - lastTicks) / 1e9;
+        lastTicks = nowTicks;
+        dt = std::min(dt, bhs::kMaxFrameTime);
+
+        solver.update(dt);
+        renderer.renderFrame(solver.cameraState(), solver.diskState(), solver.elapsedTime());
+    }
+
+    renderer.shutdown();
+    SDL_Quit();
     return 0;
 }
