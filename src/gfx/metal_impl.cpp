fbba011b55dd6955759f765f6f32d715eade1d8e
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-09-07 20:56:50 +0500

    ADD: Translation unit defining metal-cpp header implementations

---
+++ src/gfx/metal_impl.cpp
@@ -0,0 +1,9 @@
+// METAL-CPP SYMBOL DUMP. EXACTLY ONE TU MUST DO THIS. KEEP EMPTY.
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
